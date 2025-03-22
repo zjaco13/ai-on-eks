b7cf023697ce52e71c383497580b678f06c17ea6
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-03-21 17:35:31 -0700

    fix missing link
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -77,10 +77,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Get Started',
-            items: [{ label: 'Docs', to: '/docs/introduction/intro' }],
-          },
-          {
             title: 'Get Involved',
             items: [{ label: 'Github', href: 'https://github.com/awslabs/data-on-eks' }],
           },
