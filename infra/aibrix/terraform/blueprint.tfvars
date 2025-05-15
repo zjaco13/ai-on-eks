a5597511745fd3290e95da35eda988df738c48c5
Premdass Ravidass <premdaz@amazon.co.uk>
2025-05-15 16:31:20 +0100

    add aibrix through argocd

--- infra/aibrix/terraform/blueprint.tfvars
+++ infra/aibrix/terraform/blueprint.tfvars
@@ -1,2 +1,3 @@
-name                = "aibrix-on-eks"
-enable_aibrix_stack = true
+name                             = "aibrix-on-eks"
+enable_aibrix_stack              = true
+enable_argocd                    = true
--- infra/base/terraform/argocd_addons.tf
+++ infra/base/terraform/argocd_addons.tf
@@ -2,3 +2,11 @@ resource "kubectl_manifest" "ai_ml_observability_yaml" {
   count     = var.enable_ai_ml_observability_stack ? 1 : 0
   yaml_body = file("${path.module}/argocd-addons/ai-ml-observability.yaml")
 }
+resource "kubectl_manifest" "aibrix_dependency_yaml" {
+  count     = var.enable_aibrix_stack ? 1 : 0
+  yaml_body = templatefile("${path.module}/argocd-addons/aibrix-dependency.yaml",{aibrix_version=var.aibrix_stack_version})
+}
+resource "kubectl_manifest" "aibrix_core_yaml" {
+  count     = var.enable_aibrix_stack ? 1 : 0
+  yaml_body = templatefile("${path.module}/argocd-addons/aibrix-core.yaml",{aibrix_version=var.aibrix_stack_version})
+}
