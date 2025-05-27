67a89fb37670897b3634f46e3e148ff43abbf13a
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-05-27 16:15:13 -0700

    require eks blueprints to be deployed before deploying argocd addons
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

--- infra/base/terraform/argocd_addons.tf
+++ infra/base/terraform/argocd_addons.tf
@@ -1,9 +1,11 @@
 resource "kubectl_manifest" "ai_ml_observability_yaml" {
-  count     = var.enable_ai_ml_observability_stack ? 1 : 0
-  yaml_body = file("${path.module}/argocd-addons/ai-ml-observability.yaml")
+  count      = var.enable_ai_ml_observability_stack ? 1 : 0
+  yaml_body  = file("${path.module}/argocd-addons/ai-ml-observability.yaml")
+  depends_on = [module.eks_blueprints_addons]
 }
 
 resource "kubectl_manifest" "nvidia_nim_yaml" {
-  count     = var.enable_nvidia_nim_stack ? 1 : 0
-  yaml_body = file("${path.module}/argocd-addons/nvidia-nim-operator.yaml")
+  count      = var.enable_nvidia_nim_stack ? 1 : 0
+  yaml_body  = file("${path.module}/argocd-addons/nvidia-nim-operator.yaml")
+  depends_on = [module.eks_blueprints_addons]
 }
