e96ce09069eeb1e8f2c041da97017bcf15a9ec54
Premdass Ravidass <premdaz@amazon.co.uk>
2025-05-29 22:33:46 +0100

    run precommit

--- infra/base/terraform/argocd_addons.tf
+++ infra/base/terraform/argocd_addons.tf
@@ -4,13 +4,13 @@ resource "kubectl_manifest" "ai_ml_observability_yaml" {
   depends_on = [module.eks_blueprints_addons]
 }
 resource "kubectl_manifest" "aibrix_dependency_yaml" {
-  count     = var.enable_aibrix_stack ? 1 : 0
-  yaml_body = templatefile("${path.module}/argocd-addons/aibrix-dependency.yaml", { aibrix_version = var.aibrix_stack_version })
+  count      = var.enable_aibrix_stack ? 1 : 0
+  yaml_body  = templatefile("${path.module}/argocd-addons/aibrix-dependency.yaml", { aibrix_version = var.aibrix_stack_version })
   depends_on = [module.eks_blueprints_addons]
 }
 resource "kubectl_manifest" "aibrix_core_yaml" {
-  count     = var.enable_aibrix_stack ? 1 : 0
-  yaml_body = templatefile("${path.module}/argocd-addons/aibrix-core.yaml", { aibrix_version = var.aibrix_stack_version })
+  count      = var.enable_aibrix_stack ? 1 : 0
+  yaml_body  = templatefile("${path.module}/argocd-addons/aibrix-core.yaml", { aibrix_version = var.aibrix_stack_version })
   depends_on = [module.eks_blueprints_addons]
 }
 
